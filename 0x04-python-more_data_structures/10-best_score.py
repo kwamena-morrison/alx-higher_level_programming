4e3fea8a1e6e9a96797f6310b1e4a2aa38b2e21b
Kwamena-Morrison <Kwamena.morrison@yahoo.com>
2022-06-08 22:51:24 +0000

    correction of task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,4 @@
 def best_score(a_dictionary):
      if not a_dictionary:
         return None
-    biggest = 0
-    for key, value in a_dictionary.items():
-        if value > biggest:
-            biggest = value
-    for key, value in a_dictionary.items():
-        if value == biggest:
-            return key
+     return max(a_dictionary)
