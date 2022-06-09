ca9b5d137dd8ce498ccdf3c8b258718cbef407f2
Kwamena-Morrison <Kwamena.morrison@yahoo.com>
2022-06-09 00:16:05 +0000

    correction of task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
     if not roman_string:
         return 0
     if not isinstance(roman_string, str):
         return 0
     if not roman_string.isupper():
         return 0
-    # Dictionary for roman numerals
+
     r_dict = {
         "I": 1,
         "IV": 4,
